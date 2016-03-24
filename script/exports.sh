06a16c9f5542c048e1fc7384ffb89a3f25c986b1
sys_cat <systemcat91@gmail.com>
2016-03-24 19:23:48 +0900

    modify GOPATH

--- script/exports.sh
+++ script/exports.sh
@@ -1,4 +1,4 @@
 export IPADIC_NEOLOGD="`mecab-config --dicdir`/mecab-ipadic-neologd"
 export CGO_LDFLAGS="`mecab-config --libs`"
 export CGO_FLAGS="`mecab-config --inc-dir`"
-export GOPATH="$GOPATH:`pwd`/vendor"
+export GOPATH="$GOPATH:`pwd`/_vendor"
